7e8ca7f3a95410a655ff4861200d729f338e0777
Raul Vega <thedevi4nt@outlook.es>
2023-01-10 23:04:32 -0600

    Base Theme Configuration

--- app/src/main/java/com/tvmaze/challenge/ui/activities/MainActivity.kt
+++ app/src/main/java/com/tvmaze/challenge/ui/activities/MainActivity.kt
@@ -6,12 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.tvmaze.challenge.ui.screens.MainScreen
 import com.tvmaze.challenge.ui.theme.TVMazeChallengeTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,24 +20,11 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    color = MaterialTheme.colors.surface
                 ) {
-                    Greeting("Android")
+                    MainScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    TVMazeChallengeTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/tvmaze/challenge/ui/theme/Color.kt
+++ app/src/main/java/com/tvmaze/challenge/ui/theme/Color.kt
@@ -2,7 +2,8 @@ package com.tvmaze.challenge.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val PrimaryColor = Color(0xFF3C948B)
+val SecondaryColor = Color(0xFF943C45)
+val LightBlueGreen = Color(0xFFE2F2F1)
+val DarkBlueGreen = Color(0xFF2F675F)
+val DarkGray = Color(0xFF121212)
